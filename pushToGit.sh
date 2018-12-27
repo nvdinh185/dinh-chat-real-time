5956058d14fa1f99aae4131cbd8d1aa90ae59f2d
nvdinh185 <nvdinh0905671805@gmail.com>
2018-12-27 07:53:46 +0700

    push to git

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,5 +1,5 @@
-git init
+#git init
 git add .
 git commit -m "push to git"
-git remote add origin https://github.com/nvdinh185/dinh-chat-real-time.git
+#git remote add origin https://github.com/nvdinh185/dinh-chat-real-time.git
 git push -u origin master
